a1d69a5ef3652a2890e8f42840d5e466bbead1e4
minhnguyen6913 <51081204+minhnguyen6913@users.noreply.github.com>
2020-06-20 15:28:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import TopMenu from './Component/TopMenu/TopMenu.js';
-import Header from './Component/Header/Header.js';
-import Footer from './Component/Footer/Footer.js';
-import Url_router from './router/Url_router.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
 function App() {
   return (
-    <Router>
-      <div className="page">
-        <TopMenu/>
-        {/* <Header/> */}
-        <Header/>
-        <Url_router/>
-        {/* <Content src="https://i.pcmag.com/imagery/reviews/069PhzykC3xUAFsH0VKc8rG-13.fit_lpad.size_624x364.v_1569471925.jpg"/>   */}
-        {/* <Contact/> */}
-        {/* <News/> */}
-        <Footer/>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
